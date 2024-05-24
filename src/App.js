1058de423cd67f30adfec9b86561fb775c3c7be5
Rachel Wilson <rbwilson@andrew.cmu.edu>
2024-05-24 11:59:47 +0200

    start to my website as a React app?

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
 
 function App() {
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="header">
+        <img class="portrait" src="images/fys-headshot.svg" alt='portrait'/>
+        <div class="bio-info">
+            <h1 class="name">Rachel Wilson</h1>
+            <p>
+                I am pursuing a Bachelor's Degree in Computer Science at
+                Carnegie Mellon University with a minor in Hispanic Studies.
+                Beyond computer science and my love of languages, I have
+                a passion for teaching. I am fortunate enough
+                to further pursue these interests as part of the 
+                Fifth Year Scholars program in the 2024-25 academic year.
+            </p>
+            <p>
+                Some of my personal interests include crosswords, ping pong,
+                lots of television, and all things chocolate!
+            </p>
+            <p>
+                Email:
+                <a href="mailto:rbwilson@andrew.cmu.edu">
+                    <tt>rbwilson@andrew.cmu.edu</tt>
+                </a><br></br>
+                GitHub: 
+                <a href="https://github.com/rbwilson-maker">
+                    <tt>rbwilson-maker</tt>
+                </a>
+            </p>
+        </div>
     </div>
+    <div class="links">
+        <div class="link-item">
+            <a href="resume.pdf">Resume</a>
+        </div>
+        <div class="link-item">
+            <a href="https://www.cmu.edu/student-affairs/dean/fifth/index.html">Fifth Year Scholars</a>
+        </div>
+        <div class="link-item">
+            <a href="https://csd.cmu.edu/news/wilson-earns-2023-krulcik-scholarship">Krulcik Scholarship</a>
+        </div>
+        <div class="link-item">
+            <a href="../src/pets.html">Pets</a>
+        </div>
+    </div>
+    </div>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/index.css';
+import './styles/base.css';
+import './styles/main.css';
+import './styles/pets.css';
+// import './styles/bootstrap.min.css';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,4 +19,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
